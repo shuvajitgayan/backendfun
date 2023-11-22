1276b74a75e2d4b3a03d1946db975fc809fd7914
Shuvajit <shuvajitgayen57@gmail.come>
2023-11-22 14:13:59 +0530

    middlewere setup

---
+++ src/utils/cloudinaryFileUplode.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"; // fs = file system
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET 
+});
+
+const uploadOnCloudinary = async (localPath) => {
+    try {
+        if (!localPath) return null;
+        // upload file on cloudinary
+        const upload = await cloudinary.uploader.upload(localPath,{
+            resource_type:'auto'
+        });
+        // file uploaded succesfully
+        console.log("file uploaded on cloudinary ", upload.url);
+        return upload;
+    } 
+    catch (error) {
+        fs.unlinkSync(localPath) // remove the localy save temurary file as the operation failed
+        return null;
+    }
+}
+export {uploadOnCloudinary}
